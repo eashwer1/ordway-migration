ea178b884f4969a899f894b3bede6a76bf16b846
Kumar Gayatri <kumar.gayatri@ordwaylabs.com>
2022-01-14 16:23:30 -0800

    Added custom title and icon.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { json } from 'express';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, 'public'));
   app.use(json({ limit: '50mb' }));
   app.useGlobalPipes(new ValidationPipe());
 
@@ -16,8 +19,14 @@ async function bootstrap() {
     .addTag('Config MIgration')
     .addBearerAuth()
     .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  const document = SwaggerModule.createDocument(app, config, {});
+  SwaggerModule.setup('api', app, document, {
+    customSiteTitle: 'Ordway Migration',
+    customfavIcon: 'images/favicon.ico',
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
   await app.listen(process.env.PORT);
 }
 bootstrap();
