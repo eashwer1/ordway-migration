de08d7ba93c188ed818a2626eeecf7578a745766
Kumar Gayatri <kumar.gayatri@ordwaylabs.com>
2022-01-24 07:37:32 -0800

    Remove the error and update the library.

--- src/auth/jwt.auth.guard.ts
+++ src/auth/jwt.auth.guard.ts
@@ -1,6 +1,7 @@
 import { ExecutionContext, Injectable, Logger } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
+import { Observable } from 'rxjs';
 import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
 
 @Injectable()
@@ -9,12 +10,14 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
     super();
   }
 
-  canActivate(context: ExecutionContext) {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
     const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
       context.getHandler(),
       context.getClass(),
     ]);
-    Logger.log(`isPublic ${isPublic}`);
+
     if (isPublic !== true) {
       return false;
     }
